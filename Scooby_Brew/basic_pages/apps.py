e13847286520c5585a34d1738567ccffcba7f9e9
Christopher Bliss <cbliss10@gmail.com>
2019-05-13 18:25:22 -0400

    removing original files for restart in mevn

--- Scooby_Brew/basic_pages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BasicPagesConfig(AppConfig):
-    name = 'basic_pages'
--- Scooby_Brew/basic_pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from basic_pages import views
-
-app_name = 'basic_pages'
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('user_login/', views.user_login, name='user_login'),
-]
